154dd18e3700019e1c1cf15c8218ab0b2b558674
Mark Stefanovic <markstefanovic@hotmail.com>
2022-09-19 07:48:36 -0700

    cleanup ppe.job_cancel

--- setup.sql
+++ setup.sql
@@ -790,6 +790,13 @@ BEGIN
         WHERE lta.job_id = tmp.job_id
     );
 
+    DELETE FROM ppe.job_cancel AS jc
+    WHERE EXISTS (
+        SELECT 1
+        FROM tmp_ppe_jobs_to_delete AS tmp
+        WHERE jc.job_id = tmp.job_id
+    );
+
     DELETE FROM ppe.job_complete AS ji
     WHERE EXISTS (
         SELECT 1
